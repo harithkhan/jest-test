b4415e1c9aaad050636a3eb979ae8fbb2e647832
Harith Khan <184179594+harithkhan@users.noreply.github.com>
2025-02-22 00:57:19 +0800

    feat: add function and test for reverse-string

---
+++ test/reverse-string.test.js
@@ -0,0 +1,6 @@
+import { reverseString } from "../logic/reverse-string";
+
+test("String Reverse", () => {
+    expect(reverseString("apple")).toBe("elppa");
+    expect(reverseString("taylor")).toBe("rolyat");
+})
