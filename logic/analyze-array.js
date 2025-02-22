10a09534d631146976dda19c39c0cf605fe29076
Harith Khan <184179594+harithkhan@users.noreply.github.com>
2025-02-22 18:02:14 +0800

    feat: return length of arr in analyzeArray()

--- logic/analyze-array.js
+++ logic/analyze-array.js
@@ -2,10 +2,11 @@ export function analyzeArray(arr) {
     const average = arr.reduce((acc, item) => acc + item, 0) / arr.length;
     const min = Math.min(...arr);
     const max = Math.max(...arr);
+    const { length } = arr;
     return {
         average,
         min,
         max,
-        length: 6,
+        length,
     };
 }
