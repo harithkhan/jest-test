1b1d6c744c3c148ff4af8cbaf14f6032a6c731b8
Harith Khan <184179594+harithkhan@users.noreply.github.com>
2025-02-22 15:28:37 +0800

    feat: handle uppercases in caesarCipher()

--- test/caesar-cipher.test.js
+++ test/caesar-cipher.test.js
@@ -3,4 +3,10 @@ import { caesarCipher } from "../logic/caesar-cipher";
 test("Caesar Cipher: Cipher lowercase strings", () => {
     expect(caesarCipher("abc", 3)).toBe("def");
     expect(caesarCipher("def", 3)).toBe("ghi");
-})
+});
+
+test("Caeser Cipher: Cipher uppercase strings", () => {
+    expect(caesarCipher("Abc", 3)).toBe("Def");
+    expect(caesarCipher("OMG", 4)).toBe("SQK");
+    expect(caesarCipher("hIiIi", 2)).toBe("jKkKk");
+});
