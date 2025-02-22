c475b7319592bd3b12cf9c965cb4b8db822798fb
Harith Khan <184179594+harithkhan@users.noreply.github.com>
2025-02-22 16:19:27 +0800

    refactor: replace for..of loops with .forEach

--- logic/caesar-cipher.js
+++ logic/caesar-cipher.js
@@ -1,25 +1,50 @@
 function isUpperCase(letter) {
     return letter === letter.toUpperCase();
-};
+}
 
 export function caesarCipher(string, key) {
     const alphabet = [
-        "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", 
-        "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"
+        "a",
+        "b",
+        "c",
+        "d",
+        "e",
+        "f",
+        "g",
+        "h",
+        "i",
+        "j",
+        "k",
+        "l",
+        "m",
+        "n",
+        "o",
+        "p",
+        "q",
+        "r",
+        "s",
+        "t",
+        "u",
+        "v",
+        "w",
+        "x",
+        "y",
+        "z",
     ];
     const stringArray = string.split("");
     const cipheredArray = [];
-    for (let i of stringArray) {
-        for (let j of alphabet) {
-            let currentIndex;
+    stringArray.forEach((i) => {
+        alphabet.forEach((j) => {
             if (i === j || i.toLowerCase() === j) {
-                currentIndex = alphabet.indexOf(j);
+                const currentIndex = alphabet.indexOf(j);
                 const newIndex = currentIndex + key;
-                const alphabetToPush = isUpperCase(i) ? alphabet[newIndex].toUpperCase() : alphabet[newIndex];
+                const alphabetToPush = isUpperCase(i)
+                    ? alphabet[newIndex].toUpperCase()
+                    : alphabet[newIndex];
                 cipheredArray.push(alphabetToPush);
             }
-        }
-    }
+        });
+    });
     const cipheredString = cipheredArray.join("");
     return cipheredString;
 }
